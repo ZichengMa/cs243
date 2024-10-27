7c62dc36153229b404259c3208d12ebdf3747921
Ubuntu <ubuntu@ip-172-31-32-45.us-east-2.compute.internal>
2024-10-27 00:10:25 +0000

    fix doubleBinaryTree

--- hoti2024/ring-8_nodes_1_switch_topology/run.sh
+++ hoti2024/ring-8_nodes_1_switch_topology/run.sh
@@ -9,13 +9,13 @@ ASTRA_SIM=./ns3.42-AstraSimNetwork-default
 # Run ASTRA-sim
 (
 cd ${ASTRA_SIM_BUILD_DIR}
-echo "${ASTRA_SIM} \
-    --workload-configuration=${SCRIPT_DIR}/../demo1/allreduce/allreduce \
-    --system-configuration=${SCRIPT_DIR}/inputs/Ring_sys.json \
-    --remote-memory-configuration=${SCRIPT_DIR}/inputs/RemoteMemory.json \
-    --logical-topology-configuration=${SCRIPT_DIR}/inputs/logical_8nodes_1D.json \
-    --network-configuration=${SCRIPT_DIR}/../astra-sim/extern/network_backend/ns-3/scratch/config/config_8_nodes_1_switch_topology.txt \
-    --comm-group-configuration=\"empty\""
+# echo "${ASTRA_SIM} \
+#     --workload-configuration=${SCRIPT_DIR}/../demo1/allreduce/allreduce \
+#     --system-configuration=${SCRIPT_DIR}/inputs/Ring_sys.json \
+#     --remote-memory-configuration=${SCRIPT_DIR}/inputs/RemoteMemory.json \
+#     --logical-topology-configuration=${SCRIPT_DIR}/inputs/logical_8nodes_1D.json \
+#     --network-configuration=${SCRIPT_DIR}/../astra-sim/extern/network_backend/ns-3/scratch/config/config_8_nodes_1_switch_topology.txt \
+#     --comm-group-configuration=\"empty\""
 ${ASTRA_SIM} \
     --workload-configuration=${SCRIPT_DIR}/../demo1/allreduce/allreduce \
     --system-configuration=${SCRIPT_DIR}/inputs/Ring_sys.json \

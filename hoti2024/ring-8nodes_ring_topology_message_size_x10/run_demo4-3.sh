77c10a9dfb0e60e60255c718e70ba2a9006265d1
Ubuntu <ubuntu@ip-172-31-32-45.us-east-2.compute.internal>
2024-10-29 17:14:51 +0000

    add various message sizes

--- hoti2024/ring-8nodes_ring_topology_message_size_x10/run_demo4-3.sh
+++ hoti2024/ring-8nodes_ring_topology_message_size_x10/run_demo4-3.sh
@@ -24,10 +24,10 @@ ASTRA_SIM=./ns3.42-AstraSimNetwork-default
 (
 cd ${ASTRA_SIM_BUILD_DIR}
 ${ASTRA_SIM} \
-    --workload-configuration=${SCRIPT_DIR}/allreduce/allreduce \
+    --workload-configuration=${SCRIPT_DIR}/allreduce_128/allreduce \
     --system-configuration=${SCRIPT_DIR}/inputs/Ring_sys.json \
     --remote-memory-configuration=${SCRIPT_DIR}/inputs/RemoteMemory.json \
-    --logical-topology-configuration=${SCRIPT_DIR}/inputs/logical_8nodes_1D.json \
-    --network-configuration=${SCRIPT_DIR}/../astra-sim/extern/network_backend/ns-3/scratch/config/config_8nodes_ring_topology.txt \
+    --logical-topology-configuration=${SCRIPT_DIR}/inputs/logical_128nodes_1D.json \
+    --network-configuration=../../../ns-3/scratch/config/config_clos.txt \
     --comm-group-configuration=\"empty\"
 )
